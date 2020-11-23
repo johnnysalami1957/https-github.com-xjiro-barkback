26365d09d53b832e2a2fa422e7e894a713d13fa2
Chris Hylen <chris.hylen@gmail.com>
2020-11-22 23:36:44 -0600

    another update test

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 sudo mkdir /mnt/ramdisk
 sudo echo 'tmpfs /var/ramdisk tmpfs nodev,nosuid,size=100M 0 0' >> /etc/fstab
- 
+
 chmod u+x /home/pi/upwork.barkback/update.py
 
 echo 'Reboot Pi to mount ramdisk'
