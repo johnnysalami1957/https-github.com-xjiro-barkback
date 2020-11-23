581bc1e3afba468a5c3c46d18a551e521be7eec3
Chris Hylen <chris.hylen@gmail.com>
2020-11-22 21:21:51 -0600

    initial

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install sox python python-pip
+pip install -r requirements.txt
+
+sudo mkdir /mnt/ramdisk
+sudo echo 'tmpfs /var/ramdisk tmpfs nodev,nosuid,size=100M 0 0' >> /etc/fstab
+
+echo 'Reboot Pi to mount ramdisk'
